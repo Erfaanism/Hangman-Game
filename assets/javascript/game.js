4e29dc98684833eda0cb17afbf726236ea7e664e
Ali Darvishi <Erfaanism@Alis-iMac.local>
2017-08-29 01:36:27 -0700

    the initial design has and variable definition completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var arrFName = ["ARYA", "BRAN", "CERSEI", "DAENERYS", "JAMIE", "JON", "JORAH", "SANSA", "THEON", "TYRION"];
+var arrLName = ["STARK", "STARK", "LANNISTER", "TARGARYEN", "LANNISTER", "SNOW", "MORMONT", "STARK", "GREYJOY", "LANNISTER"];
+var arrImage = ["arya.jpg", "bran.jpg", "cersei.jpg", "daenerys.jpg", "jamie.jpg", "jon.jpg", "jorah.jpg", "sansa.jpg", "theon.jpg", "tyrion.jpg"];
+var cpuName = "";
+var remaining = 0;
+var usedLetters = [];
+var counterW = 0;
+var counterL = 0;
+
